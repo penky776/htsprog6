c76d5c101ce38004d4e512564220f47af5bcc1d9
penky <79907738+penky776@users.noreply.github.com>
2023-07-09 18:21:22 +0500

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, f64::consts::PI, fs::File, io::Write};
+use std::{collections::HashSet, error::Error, f64::consts::PI, fs::File, io::Write};
 
 use reqwest::header::{HeaderMap, COOKIE};
 
@@ -13,7 +13,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // insert cookies manually
     headers.insert(
         COOKIE,
-        "HackThisSite=u4ge80cge4af1hbu83drtffvp7".parse().unwrap(),
+        "HackThisSite=ocstbblq3kefj5ns4p877ca742".parse().unwrap(),
     );
 
     let res_body = client
@@ -93,7 +93,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
                 // debugging
                 // println!("{:?}", curves);
-                // println!("{:?}", lines);
+                // println!("line coordinates: {:?}", lines);
 
                 let mut all_coordinates = curves.clone();
                 all_coordinates.append(&mut lines);
@@ -112,12 +112,160 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
                 let diameter = max - min;
                 let centre_y = (diameter / 2) + min;
+
+                // get one coordinates from the first top-most character
+                let mut coordinates_max = (0, 0, 0, 0);
+                for i in all_coordinates.iter() {
+                    if i.1 == min {
+                        coordinates_max = (i.0, i.1, i.2, i.3);
+                        break;
+                    }
+                }
+
+                println!("the topmost coordinate: {:?}", coordinates_max); // debug
+
+                let all_x_values: Vec<i32> = all_coordinates.iter().map(|i| i.0).collect();
+
+                let first_char = analyze_character(
+                    all_coordinates,
+                    coordinates_max.0,
+                    coordinates_max.1,
+                    coordinates_max.2,
+                    coordinates_max.3,
+                );
+
+                println!("{:?}", first_char);
             }
         }
     }
 
     Ok(())
 }
+
+enum Character {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+struct Letter {
+    what_am_i: Character,
+    val: String,
+}
+
+struct Number {
+    what_am_i: Character,
+    val: u32,
+}
+
+fn analyze_character(
+    all_coordinates: Vec<(i32, i32, i32, i32)>,
+    initial_x: i32,
+    initial_y: i32,
+    initial_w: i32,
+    initial_h: i32,
+) -> Vec<(i32, i32, i32, i32)> {
+    let mut known_character_coordinates: Vec<(i32, i32, i32, i32)> = Vec::new();
+    known_character_coordinates.push((initial_x, initial_y, initial_w, initial_h));
+
+    'main_loop: loop {
+        let initial_len_of_known = known_character_coordinates.len();
+
+        let mut new_coordinates: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+
+        let mut already_iterated_through: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+
+        'outer: for cc in known_character_coordinates.iter() {
+            let (cc_x, cc_y, cc_w, cc_h) = (cc.0, cc.1, cc.2, cc.3);
+
+            // check if we've already iterated through cc.
+            if already_iterated_through.contains(&(cc_x, cc_y, cc_w, cc_h)) {
+                continue 'outer;
+            }
+
+            'inner: for i in all_coordinates.iter() {
+                let x = i.0;
+                let y = i.1;
+                let w = i.2;
+                let h = i.3;
+
+                // conditions
+
+                // making sure i != cc
+                if already_iterated_through.contains(&(x, y, w, h)) {
+                    continue 'inner;
+                }
+
+                if x == cc_x && y == cc_y && (w != cc_w || h != cc_h) {
+                    println!("pushing onto new_coordinates: {:?}", i);
+                    new_coordinates.insert((x, y, w, h)); // fix this
+
+                    continue 'inner;
+                }
+
+                if x == cc_x && y != cc_y && ((y - cc_y) < cc_h || (cc_y - y).abs() < cc_h) {
+                    println!("pushing onto new_coordinates: {:?}", i);
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+            }
+
+            already_iterated_through.insert((cc_x, cc_y, cc_w, cc_h));
+        }
+
+        known_character_coordinates.extend(new_coordinates.into_iter());
+
+        let new_len_of_known = already_iterated_through.len();
+
+        println!("new len of known: {:?}", new_len_of_known); // debug
+        println!("already iterated through {:?}", already_iterated_through);
+
+        if new_len_of_known == initial_len_of_known {
+            println!("breaking loop now...");
+            break 'main_loop;
+        }
+    }
+    return known_character_coordinates;
+}
+
+// TODO
+impl Character {
+    fn identify_char(&self) {
+        let methods = [Self::is_one()]; // add all methods here
+    }
+
+    fn is_one() -> bool {
+        return false;
+    }
+
+    fn is_two(
+        all_coordinates: Vec<(i32, i32, i32, i32)>,
+        curves: Vec<(i32, i32, i32, i32)>,
+        lines: Vec<(i32, i32, i32, i32)>,
+    ) -> bool {
+        return false;
+    }
+
+    fn is_a() -> bool {
+        return false;
+    }
+}
+
+// ---------------------translated the functions from the js in the source code into rust-----------------
 
 type CurveCoordinatesAndDeets = Result<Vec<(i32, i32, i32, i32)>, Box<dyn Error>>;
 
@@ -159,10 +307,10 @@ fn get_curve_coordinates(x: i32, y: i32, r: i32, s: i32, e: i32) -> CurveCoordin
 type LineCoordinatesAndDeets = Result<Vec<(i32, i32, i32, i32)>, Box<dyn Error>>;
 
 // drawLine func
-fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAndDeets {
+fn get_line_coordinates(x1_i: i32, y1_i: i32, x2_i: i32, y2_i: i32) -> LineCoordinatesAndDeets {
     let mut line_deets: Vec<(i32, i32, i32, i32)> = Vec::new();
 
-    let (mut x1, mut y1, mut x2, mut y2) = (x1, y1, x2, y2);
+    let (mut x1, mut y1, mut x2, mut y2) = (x1_i, y1_i, x2_i, y2_i);
 
     if x1 > x2 {
         let x2_initial = x2;
@@ -175,7 +323,7 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
     }
 
     let mut dx = x2 - x1;
-    let mut dy = (y2 - y2).abs();
+    let mut dy = (y2 - y1).abs();
     let mut x = x1;
     let mut y = y1;
     let y_inc = if y1 > y2 { -1 } else { 1 };
@@ -189,7 +337,7 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
         while dx > 0 {
             x += 1;
             if p > 0 {
-                line_deets.push((x_old, y, x - x_old, 1));
+                line_deets.push((x_old, y, x - x_old, 1)); // x_old, y, x - x_old, 1
                 x_old = x;
                 y += y_inc;
                 p += pru;
@@ -198,7 +346,7 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
             }
             dx -= 1;
         }
-        line_deets.push((x_old, y, x2 - x_old + 1, 1));
+        line_deets.push((x_old, y, x2 - x_old + 1, 1)); // x_old, y, x2 - x_old + 1, 1
     } else {
         let pr = dx << 1;
         let mut y_old = y;
@@ -208,7 +356,7 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
         if y2 <= y1 {
             while dy > 0 {
                 if p > 0 {
-                    line_deets.push((x + 1, y, 1, y_old - y + 1));
+                    line_deets.push((x, y, 1, y_old - y + 1)); // x++, y, 1, y_old - y + 1
                     x += 1;
                     y_old = y;
                     y += y_inc;
@@ -221,12 +369,12 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
                 dy -= 1;
             }
 
-            line_deets.push((x2, y2, 1, y_old - y2 + 1));
+            line_deets.push((x2, y2, 1, y_old - y2 + 1)); // x2, y2, 1, y_old - y2 + 1
         } else {
             while dy > 0 {
                 y += y_inc;
                 if p > 0 {
-                    line_deets.push((x + 1, y_old, 1, y - y_old));
+                    line_deets.push((x, y_old, 1, y - y_old)); // x++, y_old, 1, y - y_old
                     x += 1;
                     y_old = y;
                     p += pru;
@@ -237,7 +385,7 @@ fn get_line_coordinates(x1: i32, y1: i32, x2: i32, y2: i32) -> LineCoordinatesAn
                 dy -= 1;
             }
 
-            line_deets.push((x2, y_old, 1, y2 - y_old + 1));
+            line_deets.push((x2, y_old, 1, y2 - y_old + 1)); // x2, y_old, 1, y2 - y_old + 1
         }
     }
 
