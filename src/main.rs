eac059d434514f12a548824426aee32d7b16303c
penky <79907738+penky776@users.noreply.github.com>
2023-07-12 22:07:44 +0500

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashSet, error::Error, f64::consts::PI, fs::File, io::Write};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    f64::consts::PI,
+    fs::File,
+    io::Write,
+};
 
 use reqwest::header::{HeaderMap, COOKIE};
 
@@ -13,7 +19,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // insert cookies manually
     headers.insert(
         COOKIE,
-        "HackThisSite=ocstbblq3kefj5ns4p877ca742".parse().unwrap(),
+        "HackThisSite=pio0t98ki64gvgrd3m44o92du4".parse().unwrap(),
     );
 
     let res_body = client
@@ -95,36 +101,57 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 // println!("{:?}", curves);
                 // println!("line coordinates: {:?}", lines);
 
+                // find centre
                 let mut all_coordinates = curves.clone();
                 all_coordinates.append(&mut lines);
 
                 let all_y_values: Vec<i32> = all_coordinates.iter().map(|i| i.1).collect();
+                let all_x_values: Vec<i32> = all_coordinates.iter().map(|i| i.0).collect();
 
-                let max = all_y_values.clone().into_iter().max().unwrap(); // the 'top' value for the bottom-most character
-                let min = all_y_values.clone().into_iter().min().unwrap(); // the 'top' value for the top-most character
+                let max_y = all_y_values.clone().into_iter().max().unwrap(); // the 'top' value for the bottom-most character
+                let min_y = all_y_values.clone().into_iter().min().unwrap(); // the 'top' value for the top-most character
+
+                let max_x = all_x_values.clone().into_iter().max().unwrap();
+                let min_x = all_x_values.clone().into_iter().min().unwrap();
 
                 println!(
                     "the 'top' value for the bottom-most character: {:?}\nthe 'top' value for the top-most character: {:?}
                      ",
-                     max,
-                     min
+                     max_y,
+                     min_y
                 );
 
-                let diameter = max - min;
-                let centre_y = (diameter / 2) + min;
+                println!("the 'left' value for the right-most character: {:?}\nthe 'left' value for the left-most character: {:?}",
+                max_x, min_x
+            );
 
-                // get one coordinates from the first top-most character
+                let diameter = max_y - min_y;
+
+                let centre_x = (diameter / 2) + min_x;
+                let centre_y = (diameter / 2) + min_y;
+
+                println!("Coordinates for centre is x:{}, y:{}", centre_x, centre_y);
+
+                // get one of the coordinates from the first top-most character
                 let mut coordinates_max = (0, 0, 0, 0);
                 for i in all_coordinates.iter() {
-                    if i.1 == min {
+                    if i.1 == min_y {
                         coordinates_max = (i.0, i.1, i.2, i.3);
                         break;
                     }
                 }
 
-                println!("the topmost coordinate: {:?}", coordinates_max); // debug
+                let mut coordinates_left = (0, 0, 0, 0);
+                for i in all_coordinates.iter() {
+                    if i.0 == min_x {
+                        coordinates_left = (i.0, i.1, i.2, i.3);
+                        break;
+                    }
+                }
 
-                let all_x_values: Vec<i32> = all_coordinates.iter().map(|i| i.0).collect();
+                println!("the leftmost coordinate: {:?}", coordinates_left); // debug
+
+                println!("the topmost coordinate: {:?}", coordinates_max); // debug
 
                 let first_char = analyze_character(
                     all_coordinates,
@@ -134,12 +161,141 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     coordinates_max.3,
                 );
 
-                println!("{:?}", first_char);
+                println!("first_char: {:?}", first_char);
             }
         }
     }
 
     Ok(())
+}
+
+fn analyze_character(
+    all_coordinates: Vec<(i32, i32, i32, i32)>,
+    initial_x: i32,
+    initial_y: i32,
+    initial_w: i32,
+    initial_h: i32,
+) -> Vec<(i32, i32, i32, i32)> {
+    let mut known_character_coordinates: Vec<(i32, i32, i32, i32)> = Vec::new();
+    known_character_coordinates.push((initial_x, initial_y, initial_w, initial_h));
+
+    // preventing iteration through stuff that's already been iterated through.
+    let mut already_iterated_through: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+
+    // annotating this part in detail because i myself am not sure what i did here
+    'main_loop: loop {
+        let initial_len_of_known = known_character_coordinates.len(); // initially one, at the first loop
+
+        // the elements in this will get added to known_character_coordinates at the end of each loop
+        let mut new_coordinates: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+
+        // preventing iteration through stuff that's already been iterated through. the vector of arc coordinates have several duplicates.
+        // let mut already_iterated_through: HashSet<(i32, i32, i32, i32)> = HashSet::new();
+
+        // declare loop "outer" to loop through the known_character_coordinates vector, which gets updated each time the main loop resets
+        'outer: for cc in known_character_coordinates.iter() {
+            // declare the cc variables. cc = character coordinate (that we have already confirmed to belong to the character we are analysing)
+            let (cc_x, cc_y, cc_w, cc_h) = (cc.0, cc.1, cc.2, cc.3);
+
+            // assuming this is not our first "main" loop, check if we've already iterated through cc. if we have, then move to the next cc iteration.
+            if already_iterated_through.contains(&(cc_x, cc_y, cc_w, cc_h)) {
+                continue 'outer;
+            }
+
+            // this one loops through each coordinate that we have collected from the algorithms provided by the source code (drawArc, drawLine)
+            'inner: for i in all_coordinates.iter() {
+                let x = i.0;
+                let y = i.1;
+                let w = i.2;
+                let h = i.3;
+
+                // finding adjacent coordinates. basically a bunch of condtions are defined below to find coordinates that are "connected" with each other.
+                // if one of the conditions return true, we insert the i value into new_coordinates and move on to the next iteration of "inner" loop. this way we can test a single element of known_coordinates against all of the conditions
+                if x == cc_x && y == cc_y && (w != cc_w || h != cc_h) {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                if x == cc_x && y != cc_y && ((y - cc_y) < cc_h || (cc_y - y).abs() < cc_h) {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                // find vertical 2
+
+                // ^
+                if (cc_x == x + 1 && cc_y == y - 1) || (cc_y == y - 1 && x == cc_x + 1) {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                // upside down ^
+                if (x + 1 == cc_x && y + 1 == cc_y) || (cc_x + 1 == x && y + 1 == cc_y) {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                if ((x == cc_x - 1 || x == cc_x + 1) && w == cc_w && h == cc_h && y == cc_y)
+                    || ((y == cc_y - 1 || y == cc_y + 1) && w == cc_w && h == cc_h && x == cc_x)
+                {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                //
+                if (cc_x <= w + x && cc_x >= x) && (y <= cc_y + cc_h && y >= cc_y) {
+                    new_coordinates.insert((x, y, w, h));
+
+                    continue 'inner;
+                }
+
+                // once you know how to analyze all fifteen upright characters from the top, chances are you'll be able to easily analyze the other characters as well regardless of position.
+            }
+
+            // after we have tested the known-coordinate against all the coordinates, we insert it into already_iterated_through. if this is our first iteration of 'main' loop then we only have one element in known_coordinates so there's only one 'outer' iteration.
+            already_iterated_through.insert((cc_x, cc_y, cc_w, cc_h));
+        }
+
+        // after known_coordinates has finished iterating through itself, update the vector with the new_coordinates you have collected based on the conditions we ran the known_coordinate through above.
+        known_character_coordinates.extend(new_coordinates.into_iter());
+
+        // get the new length of known_coordinates. unless there's something seriously wrong, this variable will never contain one. this is because new_coordinates always gets updated in each loop.
+        let new_len_of_known = known_character_coordinates.len();
+
+        let already_iterated_through_len = already_iterated_through.len();
+
+        // debug
+        println!(
+            "already iterated through len: {:?}",
+            already_iterated_through_len
+        );
+        println!("new len of known: {:?}", new_len_of_known); // debug
+        println!("already iterated through {:?}", already_iterated_through);
+        // println!(
+        //     "known_character_coordinates: {:?}",
+        //     known_character_coordinates
+        // );
+
+        if new_len_of_known == initial_len_of_known {
+            println!("breaking main loop now...");
+            break 'main_loop;
+        }
+    }
+
+    /*  this variable represents the number of coordinates we have collected so far.
+    this is confusing because due to the variable name, you would expect this role to be carried by known_character_coordinates.
+    While known_character_coordinates does contain all the coordinates we have confirmed to belong to a character we are analysing,
+    the difference between already_iterated_through and known_character_coordinates is that the latter contains several duplicates
+    of the same coordinate whereas the former does not.    */
+    let collected_coordinates: Vec<(i32, i32, i32, i32)> =
+        already_iterated_through.into_iter().collect();
+
+    return collected_coordinates;
 }
 
 enum Character {
@@ -169,77 +325,6 @@ struct Letter {
 struct Number {
     what_am_i: Character,
     val: u32,
-}
-
-fn analyze_character(
-    all_coordinates: Vec<(i32, i32, i32, i32)>,
-    initial_x: i32,
-    initial_y: i32,
-    initial_w: i32,
-    initial_h: i32,
-) -> Vec<(i32, i32, i32, i32)> {
-    let mut known_character_coordinates: Vec<(i32, i32, i32, i32)> = Vec::new();
-    known_character_coordinates.push((initial_x, initial_y, initial_w, initial_h));
-
-    'main_loop: loop {
-        let initial_len_of_known = known_character_coordinates.len();
-
-        let mut new_coordinates: HashSet<(i32, i32, i32, i32)> = HashSet::new();
-
-        let mut already_iterated_through: HashSet<(i32, i32, i32, i32)> = HashSet::new();
-
-        'outer: for cc in known_character_coordinates.iter() {
-            let (cc_x, cc_y, cc_w, cc_h) = (cc.0, cc.1, cc.2, cc.3);
-
-            // check if we've already iterated through cc.
-            if already_iterated_through.contains(&(cc_x, cc_y, cc_w, cc_h)) {
-                continue 'outer;
-            }
-
-            'inner: for i in all_coordinates.iter() {
-                let x = i.0;
-                let y = i.1;
-                let w = i.2;
-                let h = i.3;
-
-                // conditions
-
-                // making sure i != cc
-                if already_iterated_through.contains(&(x, y, w, h)) {
-                    continue 'inner;
-                }
-
-                if x == cc_x && y == cc_y && (w != cc_w || h != cc_h) {
-                    println!("pushing onto new_coordinates: {:?}", i);
-                    new_coordinates.insert((x, y, w, h)); // fix this
-
-                    continue 'inner;
-                }
-
-                if x == cc_x && y != cc_y && ((y - cc_y) < cc_h || (cc_y - y).abs() < cc_h) {
-                    println!("pushing onto new_coordinates: {:?}", i);
-                    new_coordinates.insert((x, y, w, h));
-
-                    continue 'inner;
-                }
-            }
-
-            already_iterated_through.insert((cc_x, cc_y, cc_w, cc_h));
-        }
-
-        known_character_coordinates.extend(new_coordinates.into_iter());
-
-        let new_len_of_known = already_iterated_through.len();
-
-        println!("new len of known: {:?}", new_len_of_known); // debug
-        println!("already iterated through {:?}", already_iterated_through);
-
-        if new_len_of_known == initial_len_of_known {
-            println!("breaking loop now...");
-            break 'main_loop;
-        }
-    }
-    return known_character_coordinates;
 }
 
 // TODO
@@ -288,9 +373,9 @@ fn get_curve_coordinates(x: i32, y: i32, r: i32, s: i32, e: i32) -> CurveCoordin
     let mut ss = s;
 
     while ss <= s + e {
-        let ss_to_rad = ss * to_radian;
-        let xx = (x + r * (ss_to_rad.cos())).round();
-        let yy = (y - r * (ss_to_rad.sin())).round();
+        // let ss_to_rad = ss * to_radian;
+        let xx = (x + r * (ss * to_radian).cos()).round();
+        let yy = (y - r * (ss * to_radian).sin()).round();
 
         if xx != xx_last || yy != yy_last {
             curve_deets.push((xx as i32, yy as i32, 1, 1));
